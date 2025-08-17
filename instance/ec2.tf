b7d7e504df711b7fc7ce1c90fd7ac883d009a053
Nagaraj <nagarajuande255@gmail.com>
2025-08-18 00:06:37 +0530

    jenkins

--- instance/ec2.tf
+++ instance/ec2.tf
@@ -1,13 +1,13 @@
 # This script will run instance & security Group inbound rules & ourbound rules 
 resource "aws_instance" "jenkins" {
   ami           = "ami-09c813fb71547fc4f"
-  instance_type = "t3.small" # Instance type, can be changed as needed
+  instance_type = "t3.micro" # Instance type, can be changed as needed
   vpc_security_group_ids = [aws_security_group.allow_all_jenkins.id] # Security Group ID
 
   # user_data = file("jenkins.sh") # User data script to install jenkins
-  # tags = { # Tags for the instance
-  #   Name = "jenkins" # Name of the instance
-  # }
+   tags = { # Tags for the instance
+   Name = "jenkins" # Name of the instance
+   }
 }
 resource "aws_security_group" "allow_all_jenkins" {
     name        = "allow_all_jenkins"
@@ -33,12 +33,12 @@ resource "aws_security_group" "allow_all_jenkins" {
     }
 }
 
-# # Creating route 53 Record for jenkins
-# resource "aws_route53_record" "Jenkins" {
-#   zone_id         = "Z05005862BAG0R5BQ5WUP"
-#   name            = "jenkins-devops84.shop" #jenkins-dev.devops84.shop
-#   type            = "A"
-#   ttl             = 1
-#   records         = [aws_instance.jenkins.private_ip]
-#   allow_overwrite = true
-# }
+# Creating route 53 Record for jenkins
+resource "aws_route53_record" "jenkins" {
+  zone_id         = "Z05005862BAG0R5BQ5WUP" # Replace with your Route 53 hosted zone ID
+  name            = "jenkins" #jenkins-dev.devops84.shop
+  type            = "A"
+  ttl             = 1
+  records         = [aws_instance.jenkins.public_ip]
+  allow_overwrite = true
+}
