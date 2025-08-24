8ed4ac8f87b367c95318128953aac73effd39cee
Nagaraj <nagarajuande255@gmail.com>
2025-08-24 18:46:07 +0530

    jenkins

--- instance/ec2.tf
+++ instance/ec2.tf
@@ -1,12 +1,35 @@
 # This script will run instance & security Group inbound rules & ourbound rules 
 resource "aws_instance" "jenkins" {
   ami           = "ami-09c813fb71547fc4f"
-  instance_type = "t3.micro" # Instance type, can be changed as needed
+  instance_type = "t3.small" # Instance type, can be changed as needed
   vpc_security_group_ids = [aws_security_group.allow_all_jenkins.id] # Security Group ID
 
-    user_data = file("jenkins.sh") # User data script to install jenkins
+  # need more for terraform
+  root_block_device {
+    volume_size = 50
+    volume_type = "gp3" # or "gp2", depending on your preference
+  }
+  user_data = file("jenkins.sh") # User data script to install jenkins
    tags = { # Tags for the instance
     Name = "jenkins" # Name of the instance
+   }
+}
+
+
+# creating jenkins agent instance
+resource "aws_instance" "jenkins_agent" {
+  ami           = "ami-09c813fb71547fc4f"
+  instance_type = "t3.small" # Instance type, can be changed as needed
+  vpc_security_group_ids = [aws_security_group.allow_all_jenkins.id] # Security Group ID
+
+  # need more for terraform
+  root_block_device {
+    volume_size = 50
+    volume_type = "gp3" # or "gp2", depending on your preference
+  }
+  user_data = file("jenkins-agent.sh") # User data script to install jenkins
+   tags = { # Tags for the instance
+    Name = "jenkins_agent" # Name of the instance
    }
 }
 resource "aws_security_group" "allow_all_jenkins" {
@@ -33,12 +56,22 @@ resource "aws_security_group" "allow_all_jenkins" {
     }
 }
 
-# # Creating route 53 Record for jenkins
-# resource "aws_route53_record" "jenkins" {
-#   zone_id         = "Z05005862BAG0R5BQ5WUP" # Replace with your Route 53 hosted zone ID
-#   name            = "jenkins" #jenkins-dev.devops84.shop
-#   type            = "A"
-#   ttl             = 1
-#   records         = [aws_instance.jenkins.public_ip]
-#   allow_overwrite = true
-# }
+# Creating route 53 Record for jenkins
+resource "aws_route53_record" "jenkins" {
+  zone_id         = "Z05005862BAG0R5BQ5WUP" # Replace with your Route 53 hosted zone ID
+  name            = "jenkins" #jenkins-dev.devops84.shop
+  type            = "A"
+  ttl             = 1
+  records         = [aws_instance.jenkins.public_ip]
+  allow_overwrite = true
+}
+
+resource "aws_route53_record" "jenkins-agent" {
+  zone_id         = "Z05005862BAG0R5BQ5WUP" # Replace with your Route 53 hosted zone ID
+  name            = "jenkins-agent" #jenkins-dev.devops84.shop
+  type            = "A"
+  ttl             = 1
+  records         = [aws_instance.jenkins_agent.private_ip]
+  allow_overwrite = true
+}
+
--- instance/jenkins.sh
+++ instance/jenkins.sh
@@ -1,13 +1,26 @@
 #!/bin/bash
 
-# Install jenkins
-sudo curl -o /etc/yum.repos.d/jenkins.repo \
-    https://pkg.jenkins.io/redhat-stable/jenkins.repo
+#resize disk from 20GB to 50GB
+growpart /dev/nvme0n1 4
 
-sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+# Resize the logical volumes
+lvextend -L +10G /dev/RootVG/rootVol
+lvextend -L +10G /dev/mapper/RootVG-varVol
+lvextend -l +100%FREE /dev/mapper/RootVG-varTmpVol
+
+# Resize the filesystem
+xfs_growfs /
+xfs_growfs /var/tmp
+xfs_growfs /var
+
+# Install jenkins
+curl -o /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+
+rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 # Add required dependencies for the jenkins package
 
-sudo yum install fontconfig java-21-openjdk jenkins -y
-sudo systemctl daemon-reload
-sudo systemctl start jenkins
-sudo systemctl enable jenkins
+yum install fontconfig java-21-openjdk -y
+yum install jenkins -y
+systemctl daemon-reload
+systemctl start jenkins
+systemctl enable jenkins
